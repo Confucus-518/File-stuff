762d5232af111626ba245c8ab9ddf447eaeaf31c
Mconfucus <marcushh.chew@gmail.com>
2024-09-18 15:15:50 +0800

    Added a few features to make it more user-friendly

--- FIle-or-Folder.ps1
+++ FIle-or-Folder.ps1
@@ -1,12 +1,26 @@
-$folderPath = Read-Host "Enter your folder path from current directory, e.g. ../folder";
 
-$items = Get-ChildItem -Path $folderPath;
+Param (
+    [Alias("h")] [switch]$help,
+    [Alias("u")][string]$folderPath
+)
 
-foreach($file in $items){
-    if ($file.PSIsContainer){
-        Write-Output "folder:$($file.Name)";
-    }
-    else{
-        Write-Output "file:$($file.Name)";
-    }
+if ($help){
+    Write-Host "To use, simply add -folderPath or -u and folder path from current dir. E.g. -u ../folder";
 }
+
+if ($folderPath){
+    $items = Get-ChildItem -Path $folderPath;
+
+    foreach($file in $items){
+        if ($file.PSIsContainer){
+            Write-Output "folder:$($file.Name)";
+        }
+        else{
+            Write-Output "file:$($file.Name)";
+        }
+    }
+
+}elseif (-not $folderPath) {
+    Write-Host "Please insert a valid folder path using -folderPath or -u. E.g. -u ../folder";
+}
+
